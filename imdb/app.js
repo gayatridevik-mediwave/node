73a2b9db4fa4b08ff14bf3f9973d8e8ddc112c0d
gayatridevik-mediwave <gayatridevi_k@mindwaveventures.com>
2022-01-28 13:12:55 +0530

    node

--- imdb/app.js
+++ imdb/app.js
@@ -1,11 +1,24 @@
 const movies = require("./movies");
 
 const movie = {
-    id: 204,
+    id: 203,
     name: "The sword",
 };
 
+//create
 movies.push(movie);
 console.log("Added", movie.name);
 console.log(movie);
 console.log(movies);
+
+//read
+
+const id = "202";
+const movieFound = movies.find(function (m) {
+    return m.id == id;
+});
+if (!moviefound) {
+    console.log("movie ${id} not found");
+} else {
+    console.log("added", moviefound);
+}
